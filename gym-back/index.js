4e6daf569d067a61ecc95ecc881474eadb78789f
Vedant0616 <f20202047@hyderabad.bits-pilani.ac.in>
2023-04-01 05:57:48 +0530

    dont tell now it works!!!!!!!!!!

--- gym-back/index.js
+++ gym-back/index.js
@@ -2,10 +2,17 @@
 const express = require('express')
 const cors = require('cors');
 // const app = express();
-const db = require('./app/models');
+// const db = require('./app/models');
 const app = express()
 const PORT = 4000
-
+var corsOptions = {
+    origin: "https://dbms-front-uc9z.vercel.app/"
+};
+app.use(cors(corsOptions));
+// parse requests of content-type - application/json
+app.use(expresss.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(expresss.urlencoded({ extended: true }));
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
 })
@@ -17,6 +24,13 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.send('This is my about route..... ')
 })
-
+require("./app/routes/member.routes.js")(app);
+require("./app/routes/branch_manager.routes.js")(app);
+require("./app/routes/payment.routes.js")(app);
+require("./app/routes/auth.routes.js")(app);
+require("./app/routes/manager.routes.js")(app);
+require("./app/routes/equipment.routes.js")(app);
+require("./app/routes/workout.routes.js")(app);
+require("./app/routes/trainer.routes.js")(app);
 // Export the Express API
 module.exports = app
